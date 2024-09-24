7c59589a4d9751b388b9a87313ec05138c664b38
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2024-09-24 22:35:00 +0200

    create site bucket

--- static-site/index.ts
+++ static-site/index.ts
@@ -1,1 +1,25 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+
+// import the program's configuration settings.
+const config = new pulumi.Config();
+
+const indexDocument = config.get("indexDocument") || "index.html";
+const errorDocument = config.get("errorDocument") || "error.html";
+
+// create a storage bucket and configure it as a website.
+const bucket = new gcp.storage.Bucket("bucket", {
+    location: "US",
+    website: {
+        mainPageSuffix: indexDocument,
+        notFoundPage: errorDocument,
+    },
+});
+
+// create an iam binding to allow public read access to the bucket.
+const bucketIamBinding = new gcp.storage.BucketIAMBinding("bucket-iam-binding", {
+    bucket: bucket.name,
+    role: "roles/storage.objectViewer",
+    members: ["allUsers"],
+});
